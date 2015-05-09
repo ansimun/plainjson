292ec5ab34542a4719810770ba915693fb46b000
Andreas Siegemund <hausensausen@gmail.com>
2015-05-09 10:54:24 +0200

    Add new tests regarding parsing of arrays

--- tests/JsonArrayParserTests.cs
+++ tests/JsonArrayParserTests.cs
@@ -9,15 +9,15 @@ namespace Tests.Parser
     public class JsonArrayParserTests
     {
         [Test()]
-        public void ParseArray()
+        public void ParseNumberArray()
         {
             JsonParser testInstance = new JsonParser();
             ArrayList result = new ArrayList(testInstance.Parse("[0,1,2]") as IList);
             Assert.IsNotNull(result);
             Assert.AreEqual(3, result.Count);
-            Assert.AreEqual(0.0, result[0]);
-            Assert.AreEqual(1.0, result[1]);
-            Assert.AreEqual(2.0, result[2]);
+            Assert.AreEqual(0.0, result [0]);
+            Assert.AreEqual(1.0, result [1]);
+            Assert.AreEqual(2.0, result [2]);
         }
 
         [Test()]
@@ -27,6 +27,41 @@ namespace Tests.Parser
             ArrayList result = new ArrayList(testInstance.Parse("[]") as IList);
             Assert.IsNotNull(result);
             Assert.AreEqual(0, result.Count);
+        }
+
+        [Test()]
+        public void ParseStringArray()
+        {
+            JsonParser testInstance = new JsonParser();
+            ArrayList result = new ArrayList(testInstance.Parse("[\"value1\",\"value2\"]") as IList);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("value1", result [0]);
+            Assert.AreEqual("value2", result [1]);
+        }
+
+        [Test()]
+        public void ParseArrayWithContentOfDifferentTypes()
+        {
+            JsonParser testInstance = new JsonParser();
+            ArrayList result = new ArrayList(testInstance.Parse("[123,\"value2\"]") as IList);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(123.0, result [0]);
+            Assert.AreEqual("value2", result [1]);
+        }
+
+        [Test()]
+        public void ParseArrayWithWhitespaceBetweenValues()
+        {
+            JsonParser testInstance = new JsonParser();
+            ArrayList result = new ArrayList(testInstance.Parse("[ 123  ,\"value\",   50.0 ,   \" \"  ]") as IList);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Count);
+            Assert.AreEqual(123.0, result [0]);
+            Assert.AreEqual("value", result [1]);
+            Assert.AreEqual(50.0, result [2]);
+            Assert.AreEqual(" ", result [3]);
         }
     }
 }
